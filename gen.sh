414939f0860bca4be911b4f37124218921c9646d
jpark <jpark@keti.re.kr>
2025-03-30 14:07:47 +0900

    refctoring

--- gen.sh
+++
@@ -1,2 +0,0 @@
-python3 gen_original.py -n 10 --types type01 type01a type02 type03 type04 type05 -i ../dd/data/raw/ -s True
-tree outputs
---
+++ run.sh
@@ -0,0 +1,2 @@
+python3 gen.py -n 10 -o outputs -s True -w False -b False --types type01 type02 type03 type04 type05 type06 
+tree outputs
